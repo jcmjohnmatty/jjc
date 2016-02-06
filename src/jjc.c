8b53e6e18480d8c2f75c2f05a1ef72179e1d8ca5
John Matty <jcmjohnmatty@me.com>
2016-02-06 18:16:35 -0500

    Added preliminary support for jjc's command line arguments.

--- src/jjc.c
+++ src/jjc.c
@@ -1,134 +1,209 @@
-#include <lex.yy.h>
+#include <getopt.h>
+#include <stdio.h>
+
+#include <lexer.h>
 
 #include <tokens.h>
 
+static int verbose_flag = -1;
+
+static struct option long_options[] =
+  {
+	{"verbose", no_argument, 0, 'V'},
+	{"brief",   no_argument, 0, 'b'},
+	{"help",    no_argument, 0, 'h'},
+	{0, 0, 0, 0}
+  };
+
 int
 main (int argc, char* argv[]) {
+  int c;
+  char* filename;
+
+  /* Parse arguments. */
   while (1)
+    {
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "Vbh",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+		case 'V':
+		  if (verbose_flag == 0)
+			{
+			  /* ERROR */
+			}
+		  verbose_flag = 1;
+		  break;
+
+		case 'b':
+		  if (verbose_flag == 1)
+			{
+			  /* ERROR */
+			}
+		  break;
+
+		case 'h':
+		  exit (0);
+		  break;
+
+        case '?':
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
 	{
-	  int token = yylex ();
-	  printf ("%s\n", yytext);
-	  switch (token)
+	  while (optind < argc)
 		{
-		case ANDnum:
-		  break;
+		  /* Specify the input file. */
+		  FILE *infile = fopen (argv[optind], "r");
+		  ++optind;
 
-		case ASSGNnum:
-		  break;
+		  if (!infile)
+			{
+			  /* ERROR */
+			}
+		  yyin = infile;
 
-		case DECLARATIONnum:
-		  break;
+		  /* Recongnize the tokens. */
+		  while (1)
+			{
+			  int token = yylex ();
+			  printf ("%s\n", yytext);
+			  switch (token)
+				{
+				case ANDnum:
+				  break;
 
-		case DOTnum:
-		  break;
+				case ASSGNnum:
+				  break;
 
-		case ENDDECLARATIONnum:
-		  break;
+				case DECLARATIONnum:
+				  break;
 
-		case EQUALnum:
-		  break;
+				case DOTnum:
+				  break;
 
-		case GTnum:
-		  break;
+				case ENDDECLARATIONnum:
+				  break;
 
-		case IDnum:
-		  break;
+				case EQUALnum:
+				  break;
 
-		case INTnum:
-		  break;
+				case GTnum:
+				  break;
 
-		case LBRACnum:
-		  break;
+				case IDnum:
+				  break;
 
-		case LPARENnum:
-		  break;
+				case INTnum:
+				  break;
 
-		case METHODnum:
-		  break;
+				case LBRACnum:
+				  break;
 
-		case NEnum:
-		  break;
+				case LPARENnum:
+				  break;
 
-		case ORnum:
-		  break;
+				case METHODnum:
+				  break;
 
-		case PROGRAMnum:
-		  break;
+				case NEnum:
+				  break;
 
-		case RBRACnum:
-		  break;
+				case ORnum:
+				  break;
 
-		case RPARENnum:
-		  break;
+				case PROGRAMnum:
+				  break;
 
-		case SEMInum:
-		  break;
+				case RBRACnum:
+				  break;
 
-		case VALnum:
-		  break;
+				case RPARENnum:
+				  break;
 
-		case WHILEnum:
-		  break;
+				case SEMInum:
+				  break;
 
-		case CLASSnum:
-		  break;
+				case VALnum:
+				  break;
 
-		case COMMAnum:
-		  break;
+				case WHILEnum:
+				  break;
 
-		case DIVIDEnum:
-		  break;
+				case CLASSnum:
+				  break;
 
-		case ELSEnum:
-		  break;
+				case COMMAnum:
+				  break;
 
-		case EQnum:
-		  break;
+				case DIVIDEnum:
+				  break;
 
-		case GEnum:
-		  break;
+				case ELSEnum:
+				  break;
 
-		case ICONSTnum:
-		  break;
+				case EQnum:
+				  break;
 
-		case IFnum:
-		  break;
+				case GEnum:
+				  break;
 
-		case LBRACEnum:
-		  break;
+				case ICONSTnum:
+				  break;
 
-		case LEnum:
-		  break;
+				case IFnum:
+				  break;
 
-		case LTnum:
-		  break;
+				case LBRACEnum:
+				  break;
 
-		case MINUSnum:
-		  break;
+				case LEnum:
+				  break;
 
-		case NOTnum:
-		  break;
+				case LTnum:
+				  break;
 
-		case PLUSnum:
-		  break;
+				case MINUSnum:
+				  break;
 
-		case RBRACEnum:
-		  break;
+				case NOTnum:
+				  break;
 
-		case RETURNnum:
-		  break;
+				case PLUSnum:
+				  break;
 
-		case SCONSTnum:
-		  break;
+				case RBRACEnum:
+				  break;
 
-		case TIMESnum:
-		  break;
+				case RETURNnum:
+				  break;
 
-		case VOIDnum:
-		  break;
+				case SCONSTnum:
+				  break;
 
-		case EOFnum:
-		  break;
+				case TIMESnum:
+				  break;
+
+				case VOIDnum:
+				  break;
+
+				case EOFnum:
+				  break;
+				}
+			}
 		}
 	}
 }
