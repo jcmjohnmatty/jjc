e3b3cb572f2b7e78787dad38b2ba2520b81f8230
John Matty <jcmjohnmatty@me.com>
2016-03-31 18:47:49 -0400

    Initial symbol table code.

---
+++ src/symtbl.c
@@ -0,0 +1,543 @@
+/*
+ * there are three arrays for symbol table operation.  "symtbl_array" is the
+ * real symbol table, it carries all the information of an id and will still be
+ * used in code generation phase.  The attributes for an id are kept in a
+ * link list pointed by its symbol table entry. It is easy to be extended in
+ * the next phase to include more attributes, such like the size and the
+ * location of a varialbe. Below are some sample attributes of an id.  You
+ * should design your own. "stack" is a temporary structure in which all the
+ * id's in the current scoping context are visible.  "attribute_array" is to
+ * store all the attributes in symbol table.
+ */
+
+#include <stdio.h>
+
+#include <ast.h>
+#include <strtbl.h>
+#include <symtbl.h>
+
+/*
+ * symbol table array
+ */
+int symtbl_array[SYMTBL_SIZE];
+
+/*
+ * attribute array.  attributes are allocated from this array instead of from
+ * dynamically allocated space.  It may easy testing and debugging.
+ */
+attribute_type attribute_array[ATTR_SIZE];
+
+/*
+ * The stack of symbol tables.
+ */
+symtbl_stack stack[STACK_SIZE];
+
+/* stack top counter */
+int stack_top = 0;
+/* symbol table top counter */
+int symtbl_top = 0;
+/* nesting level counter */
+int nesting = 0;
+/* attribute array counter */
+int attr_top = 0;
+
+extern int yyline;
+/* string table in table.c */
+extern char strg_tbl[];
+
+void
+symtbl_init ()
+{
+  int strtbl_index;
+  int symtbl_index;
+
+  /* Get the index of the string "system". */
+  strtbl_index = loc_str("system");
+
+  if (strtbl_index != -1)
+    {
+      symtbl_index = symtbl_insert_entry (strtbl_index);
+      symtbl_set_attribute (symtbl_index, PREDE_ATTR, true);
+      symtbl_set_attribute (symtbl_index, KIND_ATTR, CLASS);
+    }
+
+  strtbl_index = loc_str("readln");
+  if (strtbl_index != -1)
+    {
+      symtbl_index = symtbl_insert_entry (strtbl_index);
+      symtbl_set_attribute (symtbl_index, NEST_ATTR, nesting + 1);
+      symtbl_set_attribute (symtbl_index, PREDE_ATTR, true);
+      symtbl_set_attribute (symtbl_index, KIND_ATTR, PROCE);
+    }
+
+  strtbl_index = loc_str("println");
+  if (strtbl_index != -1)
+    {
+      symtbl_index = symtbl_insert_entry (strtbl_index);
+      symtbl_set_attribute (symtbl_index, NEST_ATTR, nesting + 1);
+      symtbl_set_attribute (symtbl_index, PREDE_ATTR, true);
+      symtbl_set_attribute (symtbl_index, KIND_ATTR, PROCE);
+    }
+}
+
+void
+error_msg (int type, int action, int id, int symtbl_number)
+{
+  char* s;
+
+  printf ("Semantic Error--line: %d, ", yyline);
+  switch (type)
+    {
+    case STACK_OVERFLOW:
+      printf ("stack overflow.\n");
+      break;
+
+    case REDECLARATION:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: redeclared.\n", s);
+      break;
+
+    case ST_OVERFLOW:
+      printf ("symbol table overflow.\n");
+      break;
+
+    case UNDECLARATION:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: undeclared.\n", s);
+      break;
+
+    case ATTR_OVERFLOW:
+      printf ("attribute array overflowed.\n");
+      break;
+
+    case BOUND:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: not declared as a constant, can't be used as subrange bound.\n", s);
+      break;
+
+    case ARGUMENTS_NUM1:
+      printf ("routine %s: argument number in definition is different with the previous forward declaration. \n", s);
+      break;
+
+    case ARGUMENTS_NUM2:
+      s = string_table->buffer + id + 1;
+      printf ("routine %s: argument number is different with the previous declaration. \n", s);
+      break;
+
+    case FORW_REDECLARE:
+      s = string_table->buffer + id + 1;
+      printf ("routine %s: forward redeclaration.\n", s);
+      break;
+
+    case PROCE_MISMATCH:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: can't act as a procedure call.\n", s);
+      break;
+
+    case FUNC_MISMATCH:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: can't act as a function call.\n", s);
+      break;
+
+    case VAR_VAL:
+      s = string_table->buffer + id + 1;
+      printf ("routine %s: reference/value type of the ", s);
+      printf ("%s parameter different with previous forward declaration.\n",
+              symtbl_ordinal_abbreviation (symtbl_number));
+      break;
+
+    case CONSTANT_VAR:
+      s = string_table->buffer + id + 1;
+      printf ("routine %s: the ", s);
+      printf ("%s parameter is a reference argument, can't be a constant.\n",
+              symtbl_ordinal_abbreviation (symtbl_number));
+      break;
+
+    case EXPR_VAR:
+      s = string_table->buffer + id + 1;
+      printf ("routine %s: reference argument of the ", s);
+      printf ("%s parameter can't be a expression. \n",
+              symtbl_ordinal_abbreviation (symtbl_number));
+      break;
+
+    case CONSTANT_ASSIGN:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: declared to be a constant, can't be assigned a new value.\n", s);
+      break;
+
+    case ARR_TYPE_MIS:
+      s = string_table->buffer + id + 1;
+      if (symtbl_number == 0)
+        {
+          printf ("symbol %s: isn't defined as an array.\n", s);
+        }
+      else
+        {
+          printf ("symbol %s: the ", s);
+          printf ("%s index isn't defined as an array.\n",
+                  symtbl_ordinal_abbreviation (symtbl_number));
+        }
+      break;
+
+    case ARR_DIME_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: inappropriate usage of arry element.\n", s);
+      break;
+
+    case REC_TYPE_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: illegal usage of a field name. \n", s);
+      break;
+
+    case INDX_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: has incorrect number of dimensions.\n", s);
+      break;
+
+    case FIELD_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: is an undeclared field name.\n", s);
+      break;
+
+    case VARIABLE_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: can't be used as a variable.\n", s);
+      break;
+
+    case NOT_TYPE:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: is not declared as a type.\n", s);
+      break;
+
+    case TYPE_MIS:
+      s = string_table->buffer + id + 1;
+      printf ("symbol %s: incorrect type usage.\n", s);
+      break;
+
+    case MULTI_MAIN:
+      printf ("main() method already declared.\n");
+      break;
+
+    default:
+      printf ("error type: %d.\n", type);
+    }
+
+  if (action == ABORT)
+    {
+      exit (0);
+    }
+}
+
+int
+symtbl_insert_entry (int id)
+{
+  /* id is already declared in the current block. */
+  if (symtbl_lookup_here (id))
+  {
+    error_msg (REDECLARATION, CONTINUE, id, 0);
+    return 0;
+  }
+
+  if (symtbl_top >= SYMTBL_SIZE - 1)
+    {
+      error_msg (ST_OVERFLOW, ABORT, 0 ,0);
+    }
+
+  ++symtbl_top;
+  symtbl_array[symtbl_top] = 0;
+  symtbl_set_attribute (symtbl_top, NAME_ATTR, id);
+  symtbl_set_attribute (symtbl_top, NEST_ATTR, nesting);
+  symtbl_push (false, id, symtbl_top, false);
+  return symtbl_top;
+}
+
+int
+symtbl_lookup (int id)
+{
+  int i;
+
+  for (i = stack_top; i > 0; --i)
+    {
+      if (!stack[i].is_marker && stack[i].name == id)
+        {
+          stack[i].used = true;
+          return stack[i].symtbl_index;
+        }
+    }
+
+  /* id is undefined, push a dummy element onto stack. */
+  error_msg (UNDECLARATION, CONTINUE, id, 0);
+  symtbl_push (false, id, 0, true);
+  return 0;
+}
+
+int
+symtbl_lookup_here (int id)
+{
+  int i;
+
+  for (i = stack_top; !stack[i].is_marker; --i)
+    {
+      if (stack[i].name == id && !stack[i].is_dummy)
+        {
+          return stack[i].symtbl_index;
+        }
+    }
+
+  return 0;
+}
+
+void
+symtbl_open_block (void)
+{
+  ++nesting;
+  /* Push a marker onto stack. */
+  symtbl_push (true, 0, 0, false);
+}
+
+void
+symtbl_close_block (void)
+{
+  int i;
+
+  for (i = stack_top; !stack[i].is_marker; --i)
+    {
+      if (!stack[i].used && !stack[i].is_dummy)
+        {
+          /* error_msg(NOT_USED, CONTINUE, stack[i].name, 0);*/
+        }
+    }
+
+  --nesting;
+  /* trim the stack */
+  stack_top = i - 1;
+}
+
+int
+symtbl_is_attribute (int symtbl_index, int attribute_number)
+{
+  int i;
+
+  i = symtbl_array[symtbl_index];
+  while (i)
+  {
+    if (attribute_array[i].attribute_number == attribute_number)
+      {
+        return i;
+      }
+    if (attribute_array[i].attribute_number > attribute_number)
+      {
+        break;
+      }
+    i = attribute_array[i].next_attribute_index;
+  }
+  return 0;
+}
+
+int
+symtbl_get_attribute (int symtbl_index, int attribute_number)
+{
+  int i;
+
+  i = symtbl_is_attribute (symtbl_index, attribute_number);
+  if (!i)
+    {
+      printf ("DEBUG--The wanted attribute number %d does not exist\n", attribute_number);
+      return 0;
+    }
+
+  return attribute_array[i].attribute_value;
+}
+
+void
+symtbl_set_attribute (int symtbl_index, int attribute_number, int attribute_value)
+{
+  int* p;
+  int next;
+  int i;
+
+  i = symtbl_is_attribute (symtbl_index, attribute_number);
+  if (i)
+    {
+      attribute_array[i].attribute_value = attribute_value;
+      return;
+    }
+
+  p = &symtbl_array[symtbl_index];
+  next = symtbl_array[symtbl_index];
+
+  /* Search the link list for the right insert position. */
+  while (next)
+    {
+      if (attribute_array[next].attribute_number < attribute_number)
+        {
+          p = &(attribute_array[next].next_attribute_index);
+          next = attribute_array[next].next_attribute_index;
+        }
+      else
+        {
+          break;
+        }
+    }
+
+  if (attr_top >= ATTR_SIZE-1)
+    {
+      error_msg (ATTR_OVERFLOW, ABORT, 0, 0);
+    }
+
+  ++attr_top;
+  attribute_array[attr_top].attribute_number = attribute_number;
+  attribute_array[attr_top].attribute_value = attribute_value;
+  attribute_array[attr_top].next_attribute_index = next;
+  *p = attr_top;
+}
+
+char* kind_name[] = {"constant", "variable", "funcforw", "function", "ref_arg",
+                     "val_arg", "field", "typedef", "procforw", "procedure",
+                     "class", "array"};
+
+void
+symtbl_print (void)
+{
+  int i;
+  int attribute_number;
+  int attribute_value;
+  int attr_ptr;
+  int type_attr;
+  int tree_value = 0;
+  ast* tree_ptr;
+  char* s;
+
+  printf ("********************************Symbol Table************************************\n\n");
+  printf ("          Name Nest-Level  Tree-Node Predefined        Kind       Type      Value  Offset Dimension   Argnum\n\n");
+
+  for (i = 1; i <= symtbl_top; ++i)
+    {
+      tree_value = 0;
+      printf ("%3d", i);
+      for (attribute_number = NAME_ATTR; attribute_number <= ARGNUM_ATTR; ++attribute_number)
+        {
+          if (attr_ptr = symtbl_is_attribute (i, attribute_number))
+            {
+              attribute_value = attribute_array[attr_ptr].attribute_value;
+              switch (attribute_number)
+                {
+                case NAME_ATTR:
+                  s = string_table->buffer + attribute_value + 1;
+                  printf ("%11s", s);
+                  break;
+
+                case OFFSET_ATTR:
+                case NEST_ATTR:
+                case TREE_ATTR:
+                  printf ("%11d", attribute_value);
+                  break;
+
+                case TYPE_ATTR:
+                  tree_value = attribute_value;
+                  printf ("%11d", attribute_value);
+                  break;
+
+                case PREDE_ATTR:
+                  if (attribute_value == true)
+                    {
+                      printf ("%11s", "yes");
+                    }
+                  else
+                    {
+                      printf ("%11s", "no");
+                    }
+                  break;
+
+                case KIND_ATTR:
+                  printf ("%11s", kind_name[attribute_value - 1]);
+                  break;
+
+                case VALUE_ATTR:
+                  if (symtbl_get_attribute (i, KIND_ATTR) == CONST)
+                    {
+                      tree_ptr = (ast*) attribute_value;
+                      switch (NodeKind(tree_ptr))
+                        {
+                        case NUMNode:
+                          printf ("%11d", ast_get_data (tree_ptr));
+                          break;
+
+                        case CHARNode:
+                          if (isprint (ast_get_data (tree_ptr)))
+                            {
+                              printf ("%11c", ast_get_data (tree_ptr));
+                            }
+                          break;
+
+                        case STRINGNode:
+                          s = string_table->buffer + ast_get_data (tree_ptr) + 1;
+                          printf ("%11s", s);
+                          break;
+                        }
+                    }
+                  break;
+
+                case DIMEN_ATTR:
+                  printf ("%11d", attribute_value);
+                  break;
+
+                case ARGNUM_ATTR:
+                  printf ("%11d", attribute_value);
+                  break;
+                }
+            }
+          else
+            {
+              printf ("%11s", " ");
+            }
+
+          printf ("\n");
+        }
+    }
+}
+
+void
+symtbl_push (int is_marker, int name, int symtbl_index, int is_dummy)
+{
+
+  if (stack_top >= STACK_SIZE - 1)
+    {
+      error_msg (STACK_OVERFLOW, ABORT, 0, 0);
+    }
+
+  ++stack_top;
+
+  stack[stack_top].is_marker = (bool) is_marker;
+  stack[stack_top].name = name;
+  stack[stack_top].symtbl_index = symtbl_index;
+  stack[stack_top].is_dummy = (bool) is_dummy;
+  stack[stack_top].used = false;
+
+}
+
+char*
+symtbl_ordinal_abbreviation (int i)
+{
+  /** @todo This is bad, should not use local array. */
+  char s[10];
+
+  switch (i)
+    {
+    case 0:
+      return("0th");
+
+    case 1:
+      return("1st");
+
+    case 2:
+      return("2nd");
+
+    case 3:
+      return("3rd");
+
+    default:
+      sprintf (s, "%dth", i);
+      return s;
+    }
+}
---
+++ src/symtbl.h
@@ -0,0 +1,246 @@
+#ifndef __SYMTBL_H_
+#define __SYMTBL_H_
+
+#define SYMTBL_SIZE 500
+#define ATTR_SIZE 2000
+
+/*
+ * error type for error reporting routine
+ */
+
+#define STACK_OVERFLOW 100
+#define REDECLARATION 101
+#define STACK_OVERFLOW 102
+#define UNDECLARATION 103
+#define ATTR_OVERFLOW 104
+#define NOT_USED 105
+#define ARGUMENTS_NUM1 106
+#define ARGUMENTS_NUM2 107
+#define BOUND 108
+#define PROCE_MISMATCH 109
+#define VAR_VAL 110
+#define CONSTANT_VAR 111
+#define EXPR_VAR 112
+#define CONSTANT_ASSIGN 113
+#define INDX_MIS 114
+#define FIELD_MIS 115
+#define FORW_REDECLARE 116
+#define REC_TYPE_MIS 117
+#define ARR_TYPE_MIS 118
+#define VARIABLE_MIS 119
+#define FUNC_MISMATCH 120
+#define TYPE_MIS 121
+#define NOT_TYPE 122
+#define ARR_DIME_MIS 123
+#define MULTI_MAIN 124
+
+/*
+ * Processing instruction for error reporting routine.
+ */
+
+/** Print error and return to the caller. */
+#define CONTINUE 0
+/** Print the fatal error and abort execution. */
+#define ABORT 1
+
+/*
+ * These definitions correspond to the fields of a stack element.
+ */
+
+#define MARKER 1
+#define NAME 2
+#define ST_PTR 3
+#define DUMMY 4
+#define USED 5
+
+/** Value: id lexeme pointer, set by InsertEntry. */
+#define NAME_ATTR 1
+/** Value: nesting level, set by InsertEntry. */
+#define NEST_ATTR 2
+/** Value: point back to the subtree. */
+#define TREE_ATTR 3
+/** Value: is this id predefined? */
+#define PREDE_ATTR 4
+/** Value: pointer to the type tree for a varible, constant id or function. */
+#define TYPE_ATTR 6
+/** Value: the value of a constant id (integer, charater or string pointer). */
+#define VALUE_ATTR 7
+#define OFFSET_ATTR 8
+/** Value: see below. */
+#define KIND_ATTR 5
+
+#define DIMEN_ATTR   9
+#define ARGNUM_ATTR 10
+
+/*
+ * The possible values of attribute kind_attr.
+ */
+
+#define CONST 1
+#define VAR 2
+#define FUNCFORWARD 3
+#define FUNC 4
+#define REF_ARG 5
+#define VALUE_ARG 6
+#define FIELD 7
+#define TYPEDEF 8
+#define PROCFORWARD 9
+#define PROCE 10
+#define CLASS 11
+#define ARR 12
+
+/**
+ * @brief Struct of symbol table stack
+ */
+struct symtbl_stack
+{
+  /** Mark the beginning of a block. */
+  bool is_marker;
+  /** Point to the lexeme of the id. */
+  int name;
+  /** Point to the <code>id</code>'s symbol table entry. */
+  int symtbl_index;
+  /** Dummy element to indicate an undeclared <code>id</code>. */
+  bool is_dummy;
+  /** Is this <code>id</code> used? */
+  bool used;
+};
+
+typedef struct symtbl_stack symtbl_stack;
+
+/* stack array */
+extern symtbl_stack stack[STACK_SIZE];
+
+/**
+ * @brief Struct of element of attribute list.
+ */
+
+struct attribute_type
+{
+  /** Attribute number (< 256). */
+  char attribute_number;
+  /** Attribute value. */
+  int attribute_value;
+  int next_attribute_index;
+};
+
+typedef struct attribute_type attribute_type;
+
+/**
+ * @brief initialize the symbol table.
+ *
+ * Put predefined names into it and build trees for those names.
+ *
+ * @todo This routine is incomplete.
+ */
+void
+symtbl_init (void);
+
+/**
+ * @todo Write this function.
+ */
+void
+error_msg (int type, int action, int id, int symtbl_number);
+
+/**
+ * @brief Build a symbol table entry for id.
+ *
+ * The current block is searched for redeclaration error.  The id's name and
+ * nesting level attributes are set by this function.
+ */
+int
+symtbl_insert_entry (int id);
+
+/**
+ * @brief Search an id in the stack and return a pointer to the symbol table
+ *        entry.
+ *
+ * If the id is not in the stack, report error and push a dummy item on the
+ * stack so that the same error will not be reported again.
+ */
+int
+symtbl_lookup (int id);
+
+/**
+ * @brief Search an id in the stack for the current block.
+ *
+ * Returns the symbol table pointer if the id is found (if not, return 0).
+ * This routine can be used to check if there is a forward declaration for a
+ * function.
+ */
+int
+symtbl_lookup_here (int id);
+
+/**
+ * @brief Build a mark on the stack to indecate the beginning of a new block.
+ *
+ * Increment nesting level counter.  It is called when reserved words
+ * "program", "procedure", "function" or "record" are encountered.  Note:
+ * procedure or function name should be inserted into symbol table before
+ * calling this routine.
+ */
+void
+symtbl_open_block (void);
+
+/**
+ * @brief Decrement nesting level counter and remove the current block from the
+ *        stack.
+ *
+ * This function is called when exitting from a program, procedure, function or
+ * a record definition. each element is checked to see if it is used in the
+ * block.
+ */
+void
+symtbl_close_block (void);
+
+/**
+ * @brief Return the index to the searched attribute in attrarray if it is
+ *        in (nonzero).  Otherwise, return false.
+ */
+int
+symtbl_is_attribute (int symtbl_index, int attribute_number);
+
+/**
+ * @brief Return the specified attribute value for a symbol table entry if
+ *        found.  Otherwise, report an error.
+ *
+ * Note, this error is not the fault of source program but of the compiler
+ * writer.  It is printed for testing and debugging.
+ */
+int
+symtbl_get_attribute (int symtbl_index, int attribute_number);
+
+/**
+ * @brief Set attribute.
+ *
+ * If the attribute is already there, print debugging message.  Attributes for
+ * a symbol table entry are sorted by their attribute_number.
+ */
+void
+symtbl_set_attribute (int symtbl_index, int attribute_number, int attribute_value);
+
+/**
+ * @brief Pprint symbol table.
+ *
+ * @todo This routine is incomplete.
+ */
+void
+symtbl_print (void);
+
+/**
+ * @brief Push an element onto the stack.
+ */
+void
+symtbl_push (int is_marker, int name, int symtbl_index, int is_dummy);
+
+/**
+ * @brief Get the ordinal abbreviation for an integer.
+ *
+ * @param i The integer to get the ordinal abbreviation of.
+ *
+ * @return The ordinal abbreviation of <code>i</code>.
+ */
+char*
+symtbl_ordinal_abbreviation (int i);
+
+#endif /* __SYMTBL_H_ */
